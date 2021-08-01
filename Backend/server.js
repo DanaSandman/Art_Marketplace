dc863d3de2b1cabf332bd7b0fda202bda9b2adcf
dana sandman <sandmandana1@gmail.com>
2021-08-02 01:05:09 +0300

    clean server js page

--- Backend/server.js
+++ Backend/server.js
@@ -20,15 +20,9 @@ const session = expressSession({
 app.use(express.json())
 app.use(session)
 
-//זה גישה למשתנה סביבה
-// זה מנתב את הכניסה לעמוד הבית לקובץ אינדקסאיצטיאמאל צריך לייבא אותו לפבליק קודם כמו בהירוקו 
-// בפרודקשן הוא יגיש את מה שיש בפבליק
-//בפיתוח נותנים גישה ל3030
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
-    // זה בפיתוח 
-    //הוא מאפשר לדומיין בשם 8080 (שזה הקליינט למשל) לפנות לסרסר שיושב על דומיין אחר
     const corsOptions = {
         origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
         // origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000','http://localhost:3030'],
@@ -40,21 +34,18 @@ if (process.env.NODE_ENV === 'production') {
 // Access-Control-Allow-Origin: 'http://localhost:3000'
 
 
-//ריקוואייר של כל מיני דברים
 const artRoutes = require('./api/art/art.routes')
 const userRoutes = require('./api/user/user.routes')
 const authRoutes = require('./api/auth/auth.routes')
 //ROUTES*******************************
-//חיבור של הראוטים השונים
 app.use('/api/art', artRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 
-//זה אמור אם לא מצאת כלום מהראוטים פושוט תביא את הקובץ איצטיאמל שנמצא בתקיית פבליק
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
-//זה גם עוזר לעבוד בריאקט ראוטר בלי הסולמית ביואראל שפשוט יגיש את האיצטיאמל ומשם הריאקט יקח פיקוד
+
 //*************************************
 
 // const artService = require('./api/art/art.service')
