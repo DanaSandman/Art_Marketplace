b455682e27b24e9043a5c2a2335cb4ce767ea9cf
dana sandman <sandmandana1@gmail.com>
2021-12-06 18:52:19 +0200

    deploy

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const BASE_URL = process.env.NODE_ENV === 'production' ? '/api/' : '//localhost:3030/api/';
-
+// const BASE_URL = process.env.NODE_ENV === 'production' ? '/api/' : '//localhost:3030/api/';
+const BASE_URL = '/api/';
 export const httpService = {
     get(endpoint, data) {
         return ajax(endpoint, 'GET', data)
